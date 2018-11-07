017a79547e11d9fe2163bc1e8d3f435beac13bd4
EtherInc <36263855+etherinc@users.noreply.github.com>
2018-11-07 16:49:29 +0530

    adding richlist feature
    updates for richlist feature

--- tools/grabber.js
+++ tools/grabber.js
@@ -9,6 +9,7 @@ var Web3 = require('web3');
 var mongoose = require( 'mongoose' );
 var Block     = mongoose.model( 'Block' );
 var Transaction     = mongoose.model( 'Transaction' );
+var Balance     = mongoose.model( 'Balance' );
 
 var RPC_HOST = process.env.RPC_HOST || "localhost";
 var gethPort = process.env.gethPort || 8545;
@@ -17,6 +18,8 @@ var startBlockno = process.env.startBlockno || 0;
 var quiet = process.env.quiet || true;
 var terminateAtExistingDB = process.env.terminateAtExistingDB || false;
 var skipTransactions = process.env.skipTransactions || false;
+
+var web3_js = new Web3(new Web3.providers.HttpProvider('http://'+RPC_HOST+':' + gethPort.toString()));
 
 var grabBlocks = function(startBlockno) {
     var web3 = new Web3(new Web3.providers.HttpProvider('http://'+RPC_HOST+':' + gethPort.toString()));
@@ -72,6 +75,7 @@ var grabBlock = function(web3, blockNumber) {
                 }
                 else {
                     writeBlockToDB(blockData);
+                    writeBalanceToDB([blockData.miner]);
                 }
                 if (skipTransactions === false)
                     writeTransactionsToDB(blockData);
@@ -130,9 +134,10 @@ var writeBlockToDB = function(blockData) {
   */
 var checkBlockDBExistsThenWrite = function(blockData) {
     Block.find({number: blockData.number}, function (err, b) {
-        if (!b.length)
+        if (!b.length){
             writeBlockToDB(blockData);
-        else {
+            writeBalanceToDB([blockData.miner]);
+        } else {
             console.log('Aborting because block number: ' + blockData.number.toString() + 
                 ' already exists in DB.');
             process.exit(9);
@@ -147,13 +152,20 @@ var checkBlockDBExistsThenWrite = function(blockData) {
 
 var writeTransactionsToDB = function(blockData) {
     var bulkOps = [];
+    var allAddr = [];
     if (blockData.transactions.length > 0) {
         for (d in blockData.transactions) {
             var txData = blockData.transactions[d];
             txData.timestamp = blockData.timestamp;
             txData.value = etherUnits.toEther(new BigNumber(txData.value), 'wei');
             bulkOps.push(txData);
+            
+            if(!allAddr[txData.from]){ allAddr.push(txData.from); }
+            if(!allAddr[txData.to]){ allAddr.push(txData.to); }
         }
+        
+        writeBalanceToDB(allAddr);
+        
         Transaction.collection.insert(bulkOps, function( err, tx ){
             if ( typeof err !== 'undefined' && err ) {
                 if (err.code == 11000) {
@@ -170,6 +182,67 @@ var writeTransactionsToDB = function(blockData) {
                 
             }
         });
+    }
+}
+
+/**
+    Balances of address to DB
+**/
+
+var writeBalanceToDB = function(addData) {
+    if (addData.length > 0) {
+        
+        addData.forEach(function(addr){
+            var balance = 0;
+            if(web3_js.isConnected()) {
+                balance = web3_js.fromWei(web3_js.toDecimal(web3_js.eth.getBalance(addr)), 'ether');
+            }
+            
+            var insert_data = {"address": addr, "amount": balance};
+            
+            Balance.find({address: addr}, function (err, b) {
+                if (b.length){
+                    // update address balance
+                    Balance.collection.update({"address": addr}, {"$set": {"amount": balance}} , function( err, tx ){
+                        if ( typeof err !== 'undefined' && err ) {
+                            if (err.code == 11000) {
+                                console.log('Skip: Duplicate key ' + 
+                                err);
+                            } else {
+                              console.log('Error: Aborted due to error: ' + 
+                                    err);
+                              process.exit(9);
+                          }
+                        } else if(quiet === false) {
+                            console.log('DB successfully updated for balance ' +
+                                addr );
+                            
+                        }
+                    });
+                    
+                } else {
+                    
+                    // insert new address
+                    Balance.collection.insert(insert_data, function( err, tx ){
+                        if ( typeof err !== 'undefined' && err ) {
+                            if (err.code == 11000) {
+                                console.log('Skip: Duplicate key ' + 
+                                err);
+                            } else {
+                              console.log('Error: Aborted due to error: ' + 
+                                    err);
+                              process.exit(9);
+                          }
+                        } else if(quiet === false) {
+                            console.log('DB successfully written for balance ' +
+                                addr );
+                            
+                        }
+                    });
+                }
+            });
+        });
+        
     }
 }
 
